288c592895f2a987f866247a79d1cad2baf486ad
Temuujinn123 <90952413+Temuujinn123@users.noreply.github.com>
2021-11-27 09:42:12 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+ScrollReveal().reveal('.sr-left', {
+    distance: "200px",
+    origin: "left",
+    duration: 1000
+})
+
+ScrollReveal().reveal('.sr-right', {
+    distance: "200px",
+    origin: "right",
+    duration: 1000
+})
+
+ScrollReveal().reveal('#sr-top', {
+    distance: "200px",
+    origin: "top",
+    duration: 1000
+})
+
+ScrollReveal().reveal('#sr-bot', {
+    distance: "200px",
+    origin: "bottom",
+    duration: 1000
+})
